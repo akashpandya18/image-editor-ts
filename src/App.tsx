f3e78140c1b4da323cb93281060f660d554a8d98
akashpandya <akash.pandya@mindinventory.com>
2023-04-14 10:41:44 +0530

    code optimization and cleanup

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,10 @@
 import "./App.css";
-import ImageAnnot from "./components/TagAnnotation";
 import { useState } from "react";
 import FileUpload from "./utils/FileUpload";
 import Controls from "./components/controls";
-import { Blur, BrightnessUp, RotateRight, Zoom } from "./assets/icons";
-
-const controls = [
-  { id: 1, name: "Blur", type: "blur", icon: <Blur /> },
-  { id: 2, name: "Zoom", type: "zoom", icon: <Zoom /> },
-  { id: 3, name: "Rotate", type: "rotate", icon: <RotateRight /> },
-  { id: 4, name: "Brightness", type: "brightness", icon: <BrightnessUp /> },
-];
 
 function App() {
   const [imgSrc, setImgSrc] = useState("");
-  const [currentTool, setCurrentTool] = useState("tag-annot");
-  const [blur, setBlur] = useState<number>(0);
-  const [rotate, setRotate] = useState<number>(0);
-  const [brightness, setBrightness] = useState<number>(1);
 
   const onSelectFile = (e: any) => {
     if (e.target.files && e.target.files.length > 0) {
@@ -27,10 +14,6 @@ function App() {
       );
       reader.readAsDataURL(e.target.files[0]);
     }
-  };
-
-  const SetTools = () => {
-    return <></>;
   };
 
   return (
@@ -46,33 +29,7 @@ function App() {
             gap: "10px",
           }}
         >
-          <Controls
-            controls={controls}
-            blur={blur}
-            setBlur={setBlur}
-            brightness={brightness}
-            setBrightness={setBrightness}
-            rotate={rotate}
-            setRotate={setRotate}
-          />
-          <div
-            style={{
-              maxWidth: "1080px",
-              maxHeight: "700px",
-              borderRadius: "7px",
-              padding: "1rem",
-            }}
-          >
-            <ImageAnnot
-              imageSrcMain={imgSrc}
-              blur={blur}
-              setBlur={setBlur}
-              brightness={brightness}
-              setBrightness={setBrightness}
-              rotate={rotate}
-              setRotate={setRotate}
-            />
-          </div>
+          <Controls imgSrc={imgSrc} />
         </div>
       ) : (
         <FileUpload onSelectFile={onSelectFile} />
--- src/components/controls/index.tsx
+++ src/components/controls/index.tsx
@@ -1,13 +1,18 @@
 import { useState } from "react";
 import {
+  Blur,
+  BrightnessUp,
   Crop,
   Draw,
   Flip,
   More,
+  RotateRight,
   Tag,
-  TextOnImage
+  TextOnImage,
+  Zoom,
 } from "../../assets/icons";
 import UniversalSlider from "./sliders";
+import ImageAnnot from "../TagAnnotation";
 
 interface controlsType {
   id: number;
@@ -17,16 +22,17 @@ interface controlsType {
 }
 
 interface props {
-  controls: controlsType[];
-  blur: number;
-  setBlur: Function;
-  brightness: number;
-  setBrightness: Function;
-  rotate: number;
-  setRotate: Function;
+  imgSrc: string;
 }
 
-const tools = [
+const controls: controlsType[] = [
+  { id: 1, name: "Blur", type: "blur", icon: <Blur /> },
+  { id: 2, name: "Zoom", type: "zoom", icon: <Zoom /> },
+  { id: 3, name: "Rotate", type: "rotate", icon: <RotateRight /> },
+  { id: 4, name: "Brightness", type: "brightness", icon: <BrightnessUp /> },
+];
+
+const tools: controlsType[] = [
   { id: 1, name: "Tag/Annot", type: "tag-annotation", icon: <Tag /> },
   {
     id: 2,
@@ -40,132 +46,110 @@ const tools = [
   { id: 6, name: "More", type: "more", icon: <More /> },
 ];
 
-export default function Controls({
-  controls,
-  blur,
-  setBlur,
-  brightness,
-  setBrightness,
-  rotate,
-  setRotate
-}: props): JSX.Element {
+export default function Controls({ imgSrc }: props): JSX.Element {
   const [showControls, setShowControls] = useState(false);
+  const [currentTool, setCurrentTool] = useState("tag-annotation");
+  const [blur, setBlur] = useState<number>(0);
+  const [rotate, setRotate] = useState<number>(0);
+  const [brightness, setBrightness] = useState<number>(1);
+
+  function handleToolClick(key: string) {
+    switch (key) {
+      case "tag-annotation":
+        setCurrentTool("tag-annotation");
+        break;
+      case "text-on-image":
+        setCurrentTool("text-on-image");
+        break;
+      case "crop":
+        setCurrentTool("crop");
+        break;
+      case "flip":
+        setCurrentTool("flip");
+        break;
+      case "draw":
+        setCurrentTool("draw");
+        break;
+      case "more":
+        setCurrentTool("more");
+        break;
+      default:
+        break;
+    }
+  }
+
+  function ShowSelectedTool() {
+    return (
+      <>
+        {currentTool === "tag-annotation" ? (
+          <ImageAnnot
+            imageSrcMain={imgSrc}
+            blur={blur}
+            setBlur={setBlur}
+            brightness={brightness}
+            setBrightness={setBrightness}
+            rotate={rotate}
+            setRotate={setRotate}
+          />
+        ) : currentTool === "text-on-image" ? (
+          console.log("ToI")
+        ) : currentTool === "crop" ? (
+          console.log("crop")
+        ) : currentTool === "flip" ? (
+          console.log("flip")
+        ) : currentTool === "draw" ? (
+          console.log("draw")
+        ) : currentTool === "more" ? (
+          setShowControls(!showControls)
+        ) : (
+          console.log("none")
+        )}
+      </>
+    );
+  }
 
   return (
-    <div
-      style={{
-        display: "flex",
-        flexDirection: "column",
-        alignItems: "center",
-        justifyContent: "space-between",
-        height: "auto",
-        gap: "10px",
-        padding: "1rem 0",
-      }}
-    >
+    <>
       <div
         style={{
-          padding: "1rem",
-          width: "400px",
+          display: "flex",
+          flexDirection: "column",
+          alignItems: "center",
+          justifyContent: "space-between",
           height: "auto",
-          borderRadius: "7px",
-          backgroundColor: "#fafafa",
+          gap: "10px",
+          padding: "1rem 0",
         }}
       >
-        <div
-          style={{
-            display: "grid",
-            gridTemplateColumns: "repeat(3, minmax(0, 1fr))",
-            gap: "1rem",
-          }}
-        >
-          {tools.map((x: controlsType) => {
-            return (
-              <div
-                key={x.id}
-                style={{
-                  display: "flex",
-                  flexDirection: "column",
-                  alignItems: "center",
-                  justifyContent: "space-between",
-                }}
-              >
-                <div
-                  onClick={() => {
-                    switch (x.type) {
-                      case "tag-annotation":
-                        break;
-                      case "text-on-image":
-                        break;
-                      case "crop":
-                        break;
-                      case "flip":
-                        break;
-                      case "draw":
-                        break;
-                      case "more":
-                        setShowControls(!showControls);
-                        break;
-                      default:
-                        break;
-                    }
-                  }}
-                  style={{
-                    display: "block",
-                    borderRadius: "7px",
-                    backgroundColor: "#000",
-                    color: "#fff",
-                    padding: "1rem",
-                    width: "24px",
-                    cursor: "pointer",
-                    boxShadow: "0px 4px 4px 0px rgba(0, 0, 0, 0.2)",
-                  }}
-                >
-                  {x.icon}
-                </div>
-                <span
-                  style={{
-                    textAlign: "center",
-                    marginTop: "4px",
-                    color: "#3a3a3a",
-                  }}
-                >
-                  {x.name}
-                </span>
-              </div>
-            );
-          })}
-        </div>
-      </div>
-
-      {showControls && (
         <div
           style={{
             padding: "1rem",
             width: "400px",
             height: "auto",
             borderRadius: "7px",
-            backgroundColor: "#fafafa"
+            backgroundColor: "#fafafa",
           }}
         >
           <div
             style={{
               display: "grid",
-              gridTemplateColumns: "repeat(1, minmax(0, 1fr))",
-              gap: "0.5rem",
+              gridTemplateColumns: "repeat(3, minmax(0, 1fr))",
+              gap: "1rem",
             }}
           >
-            {controls.map((x: controlsType) => {
+            {tools.map((x: controlsType) => {
               return (
                 <div
                   key={x.id}
                   style={{
                     display: "flex",
-                    flexDirection: "row",
+                    flexDirection: "column",
                     alignItems: "center",
+                    justifyContent: "space-between",
                   }}
                 >
                   <div
+                    onClick={() => handleToolClick(x.type)}
                     style={{
                       display: "block",
                       borderRadius: "7px",
@@ -173,28 +157,95 @@ export default function Controls({
                       color: "#fff",
                       padding: "1rem",
                       width: "24px",
+                      cursor: "pointer",
+                      boxShadow: "0px 4px 4px 0px rgba(0, 0, 0, 0.2)",
                     }}
                   >
                     {x.icon}
                   </div>
-                  <div>
-                    <UniversalSlider
-                      label={x.name}
-                      id={x.type}
-                      blur={blur}
-                      setBlur={setBlur}
-                      brightness={brightness}
-                      setBrightness={setBrightness}
-                      rotate={rotate}
-                      setRotate={setRotate}
-                    />
-                  </div>
+                  <span
+                    style={{
+                      textAlign: "center",
+                      marginTop: "4px",
+                      color: "#3a3a3a",
+                    }}
+                  >
+                    {x.name}
+                  </span>
                 </div>
               );
             })}
           </div>
         </div>
-      )}
-    </div>
+
+        {showControls && (
+          <div
+            style={{
+              padding: "1rem",
+              width: "400px",
+              height: "auto",
+              borderRadius: "7px",
+              backgroundColor: "#fafafa",
+            }}
+          >
+            <div
+              style={{
+                display: "grid",
+                gridTemplateColumns: "repeat(1, minmax(0, 1fr))",
+                gap: "0.5rem",
+              }}
+            >
+              {controls.map((x: controlsType) => {
+                return (
+                  <div
+                    key={x.id}
+                    style={{
+                      display: "flex",
+                      flexDirection: "row",
+                      alignItems: "center",
+                    }}
+                  >
+                    <div
+                      style={{
+                        display: "block",
+                        borderRadius: "7px",
+                        backgroundColor: "#000",
+                        color: "#fff",
+                        padding: "1rem",
+                        width: "24px",
+                      }}
+                    >
+                      {x.icon}
+                    </div>
+                    <div>
+                      <UniversalSlider
+                        label={x.name}
+                        id={x.type}
+                        blur={blur}
+                        setBlur={setBlur}
+                        brightness={brightness}
+                        setBrightness={setBrightness}
+                        rotate={rotate}
+                        setRotate={setRotate}
+                      />
+                    </div>
+                  </div>
+                );
+              })}
+            </div>
+          </div>
+        )}
+      </div>
+      <div
+        style={{
+          maxWidth: "1080px",
+          maxHeight: "700px",
+          borderRadius: "7px",
+          padding: "1rem",
+        }}
+      >
+        <ShowSelectedTool />
+      </div>
+    </>
   );
-};
+}
