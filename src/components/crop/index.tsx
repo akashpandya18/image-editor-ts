35957cf6d1cb3f0990c70dfcf9a09a5265be28be
Viram Choksi <viram.choksi@mindinventory.com>
2023-04-18 14:56:41 +0530

    working on dragging

--- src/components/crop/index.tsx
+++ src/components/crop/index.tsx
@@ -3,6 +3,19 @@ import { useEffect, useRef, useState } from 'react'
 interface Props {
   imageUrl: string
 }
+interface Cropped {
+  startingX: number,
+  startingY: number,
+  height: number,
+  width: number
+}
+
+interface CropImageProps {
+  startingX: any
+  startingY: any
+  totalWidth: any
+  totalHeight: any
+}
 
 const ImageCropper = (props: Props) => {
 
@@ -12,12 +25,23 @@ const ImageCropper = (props: Props) => {
     x: 0, y: 0
   })
   const [isDragging, setIsDragging] = useState(false)
-  const [croppedImage, setCroppedImage] = useState(null)
-  const [currentCropped, setCurrentCropped] = useState()
+  const [isResize, setIsResize] = useState(false)
+  const [croppingNode, setCroppingNode] = useState<string>()
+  const [croppedImage, setCroppedImage] = useState<string>()
+  const [movedArea, setmovedArea] = useState({
+    xMoved: 0,
+    yMoved: 0
+  })
+  const [currentCropped, setCurrentCropped] = useState<Cropped>({
+    startingX: 0,
+    startingY: 0,
+    height: 0,
+    width: 0
+  })
   const [isCropping, setisCropping] = useState<boolean>(false)
 
   const canvasRef = useRef<HTMLCanvasElement>(null)
-  const imgRef = useRef<HTMLCanvasElement>(null)
+  const imgRef = useRef<HTMLImageElement>(null)
   useEffect(() => {
     const img = new Image()
     img.src = imageUrl
@@ -47,29 +71,28 @@ const ImageCropper = (props: Props) => {
     }
   }, [dimensions, imageUrl])
 
-
   const mouseDown = (event: React.MouseEvent<HTMLCanvasElement>) => {
-
     if (isCropping) {
       return // Return early if cropping is already in progress
     }
     const mouseX = event.nativeEvent.offsetX
-    let dragging = false
     const mouseY = event.nativeEvent.offsetY
 
     if (croppedImage) {
-      console.log('currentCropped', currentCropped)
-      if (mouseX > currentCropped.startingX && mouseX < currentCropped.mouseX && mouseY > currentCropped.startingY && mouseY < currentCropped.mouseY) {
+      if (mouseX > currentCropped.startingX && mouseX < currentCropped.width + currentCropped.startingX && mouseY > currentCropped.startingY &&
+        mouseY < currentCropped.height + currentCropped.startingY) {
         setIsDragging(true)
-        dragging = true
       }
+      // else if (mouseX > currentCropped.startingX - 5 && mouseX < currentCropped.startingX + 5 && mouseY > currentCropped.startingY - 5 && mouseY < currentCropped.startingY + 5) {
+      //   setIsResize(true)
+      //   setCroppingNode('n1')
+      // }
       else {
+        setIsResize(false)
         setIsDragging(false)
-
         setisCropping(true)
       }
     } else {
-
       setisCropping(true)
     }
 
@@ -77,74 +100,196 @@ const ImageCropper = (props: Props) => {
     const y = event.nativeEvent.offsetY
     setstartingNode({ x: x, y: y })
     setisCropping(true)
-    // console.log('x,y', x, y)
   }
+
+  // console.log('isResize , croppingNode', isResize, croppingNode)
 
   const mouseMove = (event: React.MouseEvent<HTMLCanvasElement>) => {
     const x = event.nativeEvent.offsetX
     const y = event.nativeEvent.offsetY
 
     if (isDragging) {
-      console.log('isDragging', startingNode)
       const canvas = canvasRef.current
       const ctx = canvas?.getContext('2d')
       const img = imgRef.current
       canvas!.width = canvas!.width // Clear the canvas and resize it
       ctx!.drawImage(img!, 0, 0, dimensions.width, dimensions.height)
-      ctx!.strokeStyle = 'red'
-      ctx!.setLineDash([10, 10])
+      ctx!.strokeStyle = 'white'
+      ctx!.setLineDash([5, 5])
       ctx!.lineWidth = 2
-      ctx?.strokeRect(x - startingNode.x, y - startingNode.y, currentCropped.width, currentCropped.height)
+      let movedArea = {
+        xMoved: currentCropped.startingX + (x - startingNode.x),
+        yMoved: currentCropped.startingY + (y - startingNode.y)
+      }
+      setmovedArea({
+        xMoved: movedArea.xMoved,
+        yMoved: movedArea.yMoved
+      })
+      ctx!.strokeRect(movedArea.xMoved, movedArea.yMoved, currentCropped.width, currentCropped.height)
+      // ctx!.strokeRect(movedArea.xMoved, startingNode.y, width, height) // Draw the rectangle using the starting node
+      ctx!.setLineDash([0, 0])
+      ctx!.beginPath()
+      ctx!.lineWidth = 3
+      ctx!.lineJoin = "round"
+      ctx!.strokeRect(movedArea.xMoved - 5, movedArea.yMoved - 5, 10, 0)
+      ctx!.strokeRect(movedArea.xMoved - 5, movedArea.yMoved - 5, 0, 10)
+      ctx!.fillStyle = "white"
+      ctx!.fill()
+      ctx!.stroke()
+      ctx!.beginPath()
+      ctx!.lineWidth = 3
+      ctx!.lineJoin = "round"
+      ctx!.strokeRect(movedArea.xMoved + currentCropped.width + 5, movedArea.yMoved - 5, -10, 0)
+      ctx!.strokeRect(movedArea.xMoved + currentCropped.width + 5, movedArea.yMoved - 5, 0, 10)
+      ctx!.fillStyle = "white"
+      ctx!.fill()
+      ctx!.stroke()
+      ctx!.beginPath()
+      ctx!.lineWidth = 3
+      ctx!.lineJoin = "round"
+      ctx!.strokeRect(movedArea.xMoved - 5, movedArea.yMoved + currentCropped.height + 5, 10, 0)
+      ctx!.strokeRect(movedArea.xMoved - 5, movedArea.yMoved + currentCropped.height + 5, 0, -10)
+      ctx!.fillStyle = "white"
+      ctx!.fill()
+      ctx!.stroke()
+      ctx!.beginPath()
+      ctx!.lineWidth = 3
+      ctx!.lineJoin = "round"
+      ctx!.strokeRect(movedArea.xMoved - 5 + currentCropped.width, movedArea.yMoved + currentCropped.height + 5, 10, 0)
+      ctx!.strokeRect(movedArea.xMoved + 5 + currentCropped.width, movedArea.yMoved + currentCropped.height + 5, 0, -10)
+      ctx!.fillStyle = "white"
+      ctx!.fill()
+      ctx!.stroke()
     }
 
-
     if (isCropping && !isDragging) {
-
-      console.log('startingNode', startingNode)
-      console.log('x,y', x, y)
       const canvas = canvasRef.current
       const ctx = canvas?.getContext('2d')
       canvas!.width = canvas!.width // Clear the canvas and resize it
       const img = imgRef.current
       ctx!.drawImage(img!, 0, 0, dimensions.width, dimensions.height) // Draw the original image again
-      ctx!.strokeStyle = 'red'
-      ctx!.setLineDash([10, 10])
+      ctx!.strokeStyle = 'white'
+      ctx!.setLineDash([5, 5])
       ctx!.lineWidth = 2
       const width = x - startingNode.x
       const height = y - startingNode.y
-      ctx?.strokeRect(startingNode.x, startingNode.y, width, height) // Draw the rectangle using the starting node
 
-      setCurrentCropped({
-        startingX: startingNode.x,
-        startingY: startingNode.y,
-        mouseX: event.nativeEvent.offsetX,
-        mouseY: event.nativeEvent.offsetY,
-        height: height,
-        width: width
-      })
+      ctx!.strokeRect(startingNode.x, startingNode.y, width, height) // Draw the rectangle using the starting node
+      ctx!.setLineDash([0, 0])
+      ctx!.beginPath()
+      ctx!.lineWidth = 3
+      ctx!.lineJoin = "round"
+      ctx!.strokeRect(startingNode.x - 5, startingNode.y - 5, 10, 0)
+      ctx!.strokeRect(startingNode.x - 5, startingNode.y - 5, 0, 10)
+      ctx!.fillStyle = "white"
+      ctx!.fill()
+      ctx!.stroke()
+      ctx!.beginPath()
+      ctx!.lineWidth = 3
+      ctx!.lineJoin = "round"
+      ctx!.strokeRect(startingNode.x + width + 5, startingNode.y - 5, -10, 0)
+      ctx!.strokeRect(startingNode.x + width + 5, startingNode.y - 5, 0, 10)
+      ctx!.fillStyle = "white"
+      ctx!.fill()
+      ctx!.stroke()
+      ctx!.beginPath()
+      ctx!.lineWidth = 3
+      ctx!.lineJoin = "round"
+      ctx!.strokeRect(startingNode.x - 5, startingNode.y + height + 5, 10, 0)
+      ctx!.strokeRect(startingNode.x - 5, startingNode.y + height + 5, 0, -10)
+      ctx!.fillStyle = "white"
+      ctx!.fill()
+      ctx!.stroke()
+      ctx!.beginPath()
+      ctx!.lineWidth = 3
+      ctx!.lineJoin = "round"
+      ctx!.strokeRect(startingNode.x - 5 + width, startingNode.y + height + 5, 10, 0)
+      ctx!.strokeRect(startingNode.x + 5 + width, startingNode.y + height + 5, 0, -10)
+      ctx!.fillStyle = "white"
+      ctx!.fill()
+      ctx!.stroke()
     }
   }
 
-  const mouseUP = (event) => {
+  document.addEventListener('mouseup', () => {
+
     if (isDragging) {
       setIsDragging(false)
     }
-    setisCropping(false)
-    const width = event.nativeEvent.offsetX - startingNode.x
-    const height = event.nativeEvent.offsetY - startingNode.y
+    if (isCropping) {
+      setisCropping(false)
+    }
+  })
+
+  const cropImage = (props: CropImageProps) => {
+
+    const { startingX, startingY, totalWidth, totalHeight } = props
+
     const canvas = canvasRef.current
-    const croppedCanvas = document.createElement('canvas')
-    const ctx = croppedCanvas.getContext('2d')
-    const xPosition = startingNode.x + (width < 0 ? width : 0)
-    const yPosition = startingNode.y + (height < 0 ? height : 0)
-    const newWidth = Math.abs(width)
-    const newHeight = Math.abs(height)
-    croppedCanvas.width = newWidth
-    croppedCanvas.height = newHeight
-    ctx!.drawImage(canvas!, xPosition + 1, yPosition + 1, newWidth - 2, newHeight - 2, 0, 0, newWidth, newHeight)
+    const ctx = canvas?.getContext('2d')
+    const img = imgRef.current
+
+    if (!img || !ctx) {
+      return
+    }
+
+    const croppedCanvas: any = document.createElement('canvas')
+    const croppedCtx = croppedCanvas.getContext('2d')
+
+    if (!croppedCtx) {
+      return
+    }
+
+    croppedCanvas.width = totalWidth
+    croppedCanvas.height = totalHeight
+
+    const imageData = ctx.getImageData(startingX + 2, startingY + 2, totalWidth - 3, totalHeight - 3)
+
+    croppedCtx.putImageData(imageData, 0, 0)
+
     setCroppedImage(croppedCanvas.toDataURL())
   }
 
+  const mouseUP = (event: any) => {
+
+    let startingX, startingY, totalWidth, totalHeight
+
+    if (isDragging) {
+      startingX = currentCropped?.startingX + (event.nativeEvent.offsetX - startingNode.x)
+      startingY = currentCropped?.startingY + (event.nativeEvent.offsetY - startingNode.y)
+      totalWidth = currentCropped?.width
+      totalHeight = currentCropped?.height
+      setCurrentCropped({
+        startingX: startingX,
+        startingY: startingY,
+        width: totalWidth,
+        height: totalHeight
+      })
+
+    }
+
+    if (isCropping && !isDragging) {
+      startingX = startingNode.x
+      startingY = startingNode.y
+      totalWidth = event.nativeEvent.offsetX - startingNode.x
+      totalHeight = event.nativeEvent.offsetY - startingNode.y
+      setCurrentCropped({
+        startingX: startingNode.x,
+        startingY: startingNode.y,
+        width: event.nativeEvent.offsetX - startingNode.x,
+        height: event.nativeEvent.offsetY - startingNode.y
+      })
+    }
+
+    cropImage({
+      startingX: startingX,
+      startingY: startingY,
+      totalWidth: totalWidth,
+      totalHeight: totalHeight
+    })
+  }
+
+  console.log('currentCropped', currentCropped)
   return (
     <>
       <canvas
@@ -153,27 +298,8 @@ const ImageCropper = (props: Props) => {
         onMouseUp={mouseUP}
         ref={canvasRef} />
       <img src={imageUrl} alt="uploaded" height={dimensions.height} width={dimensions.width} ref={imgRef} style={{ display: 'none' }} />
+      <img src={croppedImage} />
 
-      {/* <input type="file" accept="image/*" onChange={handleFileInputChange} /> */}
-      {/* {image && (
-        <>
-          <div style={{ display: 'flex', justifyContent: 'center' }}>
-            <canvas
-              ref={canvasRef}
-              onMouseDown={handleMouseDown}
-              onMouseMove={handleMouseMove}
-              onMouseUp={handleMouseUp}
-            />
-          </div>
-          <div style={{ display: 'flex', justifyContent: 'center', marginTop: '10px' }}>
-            <button onClick={handleClear}>Clear</button>
-            <button onClick={handleUndo}>Undo</button>
-
-            {croppedImage && <button onClick={handleDownload}>Download</button>}
-          </div>
-          {renderCroppedImage()}
-        </>
-      )} */}
     </>
   )
 }
