e6b375c689ceb9fe5393b85c93f27a95d28fcf74
akashpandya18 <124558539+akashpandya18@users.noreply.github.com>
2023-04-20 12:09:30 +0530

    Restructuring (#2)
    * 🧪 structural changes
    
    * 🧪 restructuring 2
    
    * ⚡ updated port number and cleared tag annotations
    
    * ⚡ major restructuring done

--- src/components/draw/index.tsx
+++ src/components/draw/index.tsx
@@ -1,58 +1,30 @@
-import React from "react";
-import { useOnDraw } from "../../hooks/useOnDraw";
-
-interface CanvasProps {
-  width: number;
-  height: number;
+export function onDraw(
+  ctx: CanvasRenderingContext2D,
+  point: { x: number; y: number },
+  prevPoint: { x: number; y: number } | null
+) {
+  drawLine(prevPoint, point, ctx, "#000000", 5);
 }
 
-const Draw: React.FC<CanvasProps> = ({ width, height }) => {
-  const { setCanvasRef, onCanvasMouseDown } = useOnDraw(onDraw);
+function drawLine(
+  start: { x: number; y: number } | null,
+  end: { x: number; y: number },
+  ctx: CanvasRenderingContext2D,
+  color: string,
+  width: number
+) {
+  start = start ?? end;
+  ctx.beginPath();
+  ctx.lineWidth = width;
+  ctx.strokeStyle = color;
+  if (start) {
+    ctx.moveTo(start.x, start.y);
+    ctx.lineTo(end.x, end.y);
+    ctx.stroke();
 
-  function onDraw(
-    ctx: CanvasRenderingContext2D,
-    point: { x: number; y: number },
-    prevPoint: { x: number; y: number } | null
-  ) {
-    drawLine(prevPoint, point, ctx, "#000000", 5);
-  }
-
-  function drawLine(
-    start: { x: number; y: number } | null,
-    end: { x: number; y: number },
-    ctx: CanvasRenderingContext2D,
-    color: string,
-    width: number
-  ) {
-    start = start ?? end;
+    ctx.fillStyle = color;
     ctx.beginPath();
-    ctx.lineWidth = width;
-    ctx.strokeStyle = color;
-    if (start) {
-      ctx.moveTo(start.x, start.y);
-      ctx.lineTo(end.x, end.y);
-      ctx.stroke();
-
-      ctx.fillStyle = color;
-      ctx.beginPath();
-      ctx.arc(start.x, start.y, 2, 0, 2 * Math.PI);
-      ctx.fill();
-    }
+    ctx.arc(start.x, start.y, 2, 0, 2 * Math.PI);
+    ctx.fill();
   }
-
-  return (
-    <canvas
-      width={width}
-      height={height}
-      onMouseDown={onCanvasMouseDown}
-      style={canvasStyle}
-      ref={(ref) => setCanvasRef(ref)}
-    />
-  );
-};
-
-export default Draw;
-
-const canvasStyle = {
-  border: "1px solid black",
-};
+}
--- src/utils/data.tsx
+++ src/utils/data.tsx
@@ -5,25 +5,20 @@ import {
   Crop,
   Draw,
   Flip,
+  More,
   RotateRight,
   Tag,
   TextOnImage,
   Zoom,
 } from "../assets/icons";
+import { controlsType } from "../types";
 
-interface controlsType {
-  id: number;
-  name: string;
-  type: string;
-  icon: any;
-}
 export const controls: controlsType[] = [
   { id: 1, name: "Blur", type: "blur", icon: <Blur /> },
   { id: 2, name: "Zoom", type: "zoom", icon: <Zoom /> },
   { id: 3, name: "Rotate", type: "rotate", icon: <RotateRight /> },
   { id: 4, name: "Brightness", type: "brightness", icon: <BrightnessUp /> },
 ];
-
 export const tools: controlsType[] = [
   { id: 1, name: "Tag/Annot", type: "tag-annotation", icon: <Tag /> },
   {
@@ -35,8 +30,8 @@ export const tools: controlsType[] = [
   { id: 3, name: "Crop", type: "crop", icon: <Crop /> },
   { id: 4, name: "Flip", type: "flip", icon: <Flip /> },
   { id: 5, name: "Draw", type: "draw", icon: <Draw /> },
+  { id: 6, name: "More", type: "more", icon: <More /> },
 ];
-
 export function handleToolClick(
   key: string,
   index: number,
@@ -47,25 +42,37 @@ export function handleToolClick(
   setCurrentTool: {
     (value: SetStateAction<string>): void;
     (arg0: string): void;
+  },
+  setCurrentControl: {
+    (value: SetStateAction<string>): void;
+    (arg0: string): void;
   }
 ) {
   setActiveIndex(index);
   switch (key) {
     case "tag-annotation":
       setCurrentTool("tag-annotation");
+      setCurrentControl("tag-annotation");
       break;
     case "text-on-image":
       setCurrentTool("text-on-image");
+      setCurrentControl("text-on-image");
       break;
     case "crop":
       setCurrentTool("crop");
+      setCurrentControl("crop");
       break;
     case "flip":
       setCurrentTool("flip");
+      setCurrentControl("flip");
       break;
     case "draw":
       setCurrentTool("draw");
+      setCurrentControl("draw");
       break;
+    case "more":
+      setCurrentTool("more");
+      setCurrentControl("more");
     default:
       break;
   }
